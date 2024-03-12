d5e3da6cfc3ce95187b578f4e08afabd4397fda4
Norayio <pierresediaz@gmail.com>
2024-03-12 16:35:01 -0500

    getters & setters creados

--- Inventario/src/modelo/Producto.java
+++ Inventario/src/modelo/Producto.java
@@ -21,6 +21,48 @@ public class Producto {
         this.inventario = inventario;
     }
     
+    // constructor para actualizar
+    public Producto(int codigo, String nombre, double precio, int inventario) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.inventario = inventario;
+    }
+
+    // getters & setters
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(int inventario) {
+        this.inventario = inventario;
+    }
+    
     
     
     
